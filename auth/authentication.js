69ce773e7da77c78b27fab12ddbfe3b9cb20c83b
ChaahatL <chaahatlokhande@gmail.com>
2025-03-16 17:53:20 +0530

    prepare for release

--- auth/authentication.js
+++ auth/authentication.js
@@ -2,18 +2,29 @@ const jwt = require('jsonwebtoken');
 
 const VerifyAuth = (req, res, next) => {
     console.log(req.headers.authorization);
-    var token = req.headers.authorization.slice(7, req.headers.authorization.length);
-    try{
-        jwt.verify(token,'secret',(_err,decoded) => {
+
+    if (!req.headers.authorization) {  
+        res.status(401);
+        return res.send('Auth Failed: No Token Provided');  
+    }
+
+    var token = req.headers.authorization.slice(7); // Only execute if authorization exists
+
+    try {
+        jwt.verify(token, 'secret', (err, decoded) => {
+            if (err) {
+                res.status(403);
+                return res.send('Auth Failed: Invalid Token');
+            }
             res.locals.username = decoded.username;
             res.locals.role = decoded.role;
             console.log(decoded);
             next();
         });
-    } catch (err){
+    } catch (err) {
         res.status(401);
         res.send('Auth Failed');
     }
-}
+};
 
 module.exports = VerifyAuth;
